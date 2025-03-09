f40f2c9d200b6bd22bd36b573927c086f1494f1e
kght6123 <i.feeling.song+github@gmail.com>
2025-03-09 23:48:17 +0900

    initial commit.

--- browser_use_sample.py
+++ browser_use_sample.py
@@ -4,9 +4,16 @@ import asyncio
 from dotenv import load_dotenv
 load_dotenv()
 
+# task_prompt = "エリンギ、まいたけ、キャベツ、じゃがいも、とりむね肉、鶏もも肉、豚肉があります。この材料からいくつか使って、簡単な晩御飯のおかずを日本語で考えてください。3歳から6歳の子供が喜んで食べるものでお願いします。"
+# task_prompt = "CSSの`@layer`ディレクティブについて、初心者向けに使い方とメリット、デメリットを教えてください。"
+# task_prompt = "CSSの`@layer`ディレクティブについて、初心者向けに使い方をわかりやすいサンプルコード付きで教えてください。"
+# task_prompt = "CSSの`color-mix`関数について、初心者向けに使い方とメリット、デメリットを教えてください。"
+# task_prompt = "CSSの`color-mix`関数について、初心者向けに使い方をわかりやすいサンプルコード付きで教えてください。"
+task_prompt = "CSSの`@property`ディレクティブについて、syntaxやinherits、initial-valueを使った場合のグラデーションのアニメートや大きなページでのパフォーマンス向上について、初心者向けに使い方をわかりやすいサンプルコード付きで教えてください。"
+
 async def main():
     agent = Agent(
-        task="エリンギ、まいたけ、キャベツ、じゃがいも、とりむね肉、鶏もも肉、豚肉があります。この材料からいくつか使って、簡単な晩御飯のおかずを日本語で考えてください。3歳から6歳の子供が喜んで食べるものでお願いします。",
+        task=task_prompt,
         llm=ChatOpenAI(model="gpt-4o-mini"),
     )
     await agent.run()
--- deep_research.py
+++ deep_research.py
@@ -2,10 +2,15 @@ from gpt_researcher import GPTResearcher
 from gpt_researcher.utils.enum import ReportType, Tone
 import asyncio
 
+# query_prompt = "エリンギ、まいたけ、キャベツ、じゃがいも、とりむね肉、鶏もも肉、豚肉があります。この材料からいくつか使って、簡単な晩御飯のおかずを日本語で考えてください。3歳から6歳の子供が喜んで食べるものでお願いします。"
+# query_prompt = "CSSの`@layer`ディレクティブについて、初心者向けに使い方とメリット、デメリットを教えてください。"
+# query_prompt = "CSSの`color-mix`関数について、初心者向けに使い方とメリット、デメリットを教えてください。"
+query_prompt = "CSSの`@property`ディレクティブについて、syntaxやinherits、initial-valueを使った場合のグラデーションのアニメートや大きなページでのパフォーマンス向上について、初心者向けに使い方をわかりやすいサンプルコード付きで教えてください。"
+
 async def main():
     # Initialize researcher with deep research type
     researcher = GPTResearcher(
-        query="エリンギ、まいたけ、キャベツ、じゃがいも、とりむね肉、鶏もも肉、豚肉があります。この材料からいくつか使って、簡単な晩御飯のおかずを日本語で考えてください。3歳から6歳の子供が喜んで食べるものでお願いします。",
+        query=query_prompt,
         report_type="deep",  # This triggers deep research modd
     )
     
